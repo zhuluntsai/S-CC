907685cdeb27406a3204778d6f475d02c92d5e37
zhuluntsai <william041107@gmail.com>
2023-10-10 20:28:17 -0500

    filter update

--- noise_reduct.py
+++ noise_reduct.py
@@ -10,76 +10,55 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import cv2
 from pycocotools.coco import COCO
+import matplotlib.cm as cm
+import copy
 
 label_dict = {
-    -1: 'all',
-    0: 'background',
-    2: 'buildings',
-    4: 'grass',
+    # -1: 'all',
+    # 0: 'background',
+    # 2: 'buildings',
+    # 4: 'grass',
     6: 'pedestrian walk',
     8: 'trees', 
 }
 
 def IQR(data, iteration):
-    q75, q25 = np.percentile(data, [75, 25])
-    median = np.median(data)
-    # data = np.array([ d if d > q25 and d < q75 else median for d in data ])
-    data = np.array([ d for d in data if d > q25 and d < q75 ])
+    temp = [d for d in data if d != 0]
+    q75, q25 = np.percentile(temp, [75, 25])
+    median = np.median(temp)
+    data = np.array([ d if d > q25 and d < q75 else 0 for d in data ])
+    # data = np.array([ d for d in data if d > q25 and d < q75 ])
     return data
 
 def standard_deviation(data, sigma):
     temp = [d for d in data if d != 0]
     std = np.std(temp)
     mean = np.mean(temp)
-    print(std, mean)
     data = np.array([ d if abs(d - mean) < sigma * std else 0 for d in data ])
     # data = np.array([ d for d in data if abs(d - mean) < sigma * std ])
     return data
 
-def check_outlier(point_data, label):
+def check_outlier(data, label):
     label_name = label_dict[label]
+    binwidth = 0.1
+    sigma = 1
+    data = [d for d in data.ravel() if d != 0]
+    bins = np.arange(np.min(data), np.max(data) + binwidth, binwidth)
 
-    xlim_min = 5
-    xlim_max = 35
-    bins = np.linspace(xlim_min, xlim_max, (xlim_max - xlim_min) * 30)
-    
-    xlim_zoom_min = 9
-    xlim_zoom_max = 12
-
-    lowerbound = 0
-    upperbound = 10.3
-    if label in [2, 8]: 
-        lowerbound = upperbound
-        upperbound = 35
-    xlim_remove_min = max(lowerbound, xlim_min)
-    xlim_remove_max = min(upperbound, xlim_max)
-    bins_remove = np.linspace(xlim_remove_min, xlim_remove_max, int(xlim_remove_max - xlim_remove_min) * 30)
-
-    height = [ point[2] for point in point_data if point[3] == label]
     fig = plt.figure(dpi=500)
-    fig.suptitle(f'label: {label_name}, point amount: {len(height)}')
+    fig.suptitle(f'label: {label_name}, point amount: {len(data)}')
     plt.subplots_adjust(hspace=1.2)
 
-    ax1 = plt.subplot(311)
-    ax1.title.set_text('original')
-    plt.hist(height, bins=bins)
-    plt.axvspan(xlim_zoom_min, xlim_zoom_max, color='orange', alpha=0.5)
-    plt.xlim(xlim_min, xlim_max)
-    plt.xlabel('z (height)')
-    
-    ax2 = plt.subplot(312)
-    ax2.title.set_text('zoom in')
-    plt.hist(height, bins=bins, color='orange')
-    plt.xlim(xlim_zoom_min, xlim_zoom_max)
-    plt.xticks(np.arange(xlim_zoom_min, xlim_zoom_max, step=0.2), rotation=45)
+    ax1 = plt.subplot(211)
+    ax1.title.set_text(f'original')
+    plt.hist(data, bins=bins)
     plt.xlabel('z (height)')
 
-    ax3 = plt.subplot(313)
-    ax3.title.set_text('remove outliers')
-    height_remove =  [ point[2] for point in point_data if point[3] == label and point[2] > lowerbound and point[2] < upperbound]
-    plt.hist(height, bins=bins)
-    plt.hist(height_remove, bins=bins_remove, color='orange')
-    plt.xlim(xlim_min, xlim_max)
+    data = standard_deviation(data, sigma)
+    data = standard_deviation(data, sigma)
+    ax2 = plt.subplot(212, sharex=ax1, sharey=ax1)
+    ax2.title.set_text(f'mask, sigma:{sigma}')
+    plt.hist(data, bins=bins)
     plt.xlabel('z (height)')
 
     plt.savefig(f'output/outlier_{label}.png')
@@ -122,22 +101,32 @@ def check_mask(dsm_array, filename):
     v = np.linspace(dsm_array.min(), dsm_array.max(), 15, endpoint=True)
     fig.colorbar(im, ticks=v)
     plt.savefig(filename)
+    print(f'{filename} are saved')
 
+def check_boxplot(elevation, label):
+    box_list = [ [ e for e, l in zip(elevation.ravel(), label.ravel()) if l == k] for k in list(label_dict.keys()) ]
+    
+    fig = plt.figure(dpi=500)
+    ax1 = plt.subplot(111)
+    plt.boxplot(box_list, labels=list(label_dict.values()))
+    plt.savefig(f'output/outlier_boxplot.png')
 
-def batch(x_list):
-    for i, x in enumerate(x_list):
-        try:
-            if x == 0:
-                x_list[i] = (x_list[i - 1] + x_list[i + 1]) / 2
-        except:
-            pass
-    return x_list
+    elevation = IQR(elevation.ravel(), 1).reshape(elevation.shape)
+    fig, ax = plt.subplots(dpi=500)
+    im = ax.imshow(elevation, cmap='viridis', vmin=elevation.min(), vmax=elevation.max())
+    v = np.linspace(elevation.min(), elevation.max(), 15, endpoint=True)
+    fig.colorbar(im, ticks=v)
+    plt.savefig(f'output/test.png')
 
-def boxfilter(elevation, label):
+def boxfilter(elevation, label, sigma):
     height, width = elevation.shape
+    temp = [d for d in elevation.ravel() if d != 0]
+    std = np.std(temp)
+    mean = np.mean(temp)
 
-    kernel = np.ones((7, 7))
+    kernel = np.ones((5, 5))
     kernel_center = [int(kernel.shape[0]/2), int(kernel.shape[1]/2)]
+    count = [0, 0]
 
     new_image = np.zeros(elevation.shape)
     for x in range(0, width, 1):
@@ -145,6 +134,11 @@ def boxfilter(elevation, label):
             continue
         for y in range(0, height, 1):
             pixel = elevation[y, x]
+
+            if mean - std * sigma < pixel and pixel < mean + std * sigma:
+                new_image[y, x] = pixel
+                count[0] += 1
+                continue
 
             kernel_list = []
             for i in range(0, kernel.shape[0], 1):
@@ -170,58 +164,48 @@ def mask_filter(elevation, label):
     coco = COCO(json_file)
 
     img = coco.imgs[1]
-    sigma = 5
+    sigma = 0.5
 
-    # anns_ids = coco.getAnnIds(imgIds=img['id'], catIds=2, iscrowd=None)
-    anns_ids = coco.getAnnIds(imgIds=img['id'], iscrowd=None)
-    anns = coco.loadAnns(anns_ids)
+    for k in list(label_dict.keys()):
+        anns_ids = coco.getAnnIds(imgIds=img['id'], catIds=k, iscrowd=None)
+        anns = coco.loadAnns(anns_ids)
 
-    # max_area = 0
-    # max_anns = anns[0]
-    # for annotation in anns:
-    #     area = annotation['area']
-    #     if area > max_area:
-    #         max_area = area
-    #         max_anns = annotation
+        max_area = 0
+        max_anns = anns[0]
+        for annotation in anns:
+            area = annotation['area']
+            if area > max_area:
+                max_area = area
+                max_anns = annotation
 
-    blank_mask = np.zeros((img['height'], img['width']))
-    for annotation in tqdm(anns):
-        mask = coco.annToMask(annotation)
-        mask_elevation = elevation * mask
-        blank_mask += boxfilter(mask_elevation, label) * mask
-        # blank_mask += mask_elevation
-        # mask_elevation = standard_deviation(mask_elevation.ravel(), sigma).reshape(mask_elevation.shape)
+        blank_mask = np.zeros((img['height'], img['width']))
+        mask = coco.annToMask(anns[0])    
+        for annotation in tqdm(anns):
+            mask = coco.annToMask(max_anns)
+            mask_elevation = elevation * mask
+            mask_elevation = boxfilter(mask_elevation, label, sigma) * mask
+            # blank_mask += mask_elevation
+            # mask_elevation = standard_deviation(mask_elevation.ravel(), sigma).reshape(mask_elevation.shape)
+            # check_mask(mask_elevation, f'output/{label_dict[catid]}_before.png')
+            check_mask(mask_elevation, f'output/{label_dict[k]}_after.png')
+            check_outlier(mask_elevation, k)
         
-    check_mask(blank_mask, 'output/test_all.png')
+            exit()
 
-    # label_name = label_dict[2]
-    # binwidth = 0.1
-    
-    # height = [h for h in elevation.flatten() if h != 0]
-    # bins = np.arange(np.min(height), np.max(height) + binwidth, binwidth)
-    # print(bins)
-
-    # fig = plt.figure(dpi=500)
-    # fig.suptitle(f'label: {label_name}, point amount: {len(height)}')
-    # plt.subplots_adjust(hspace=1.2)
-
-    # mask = coco.annToMask(max_anns)
-    # ax1 = plt.subplot(211)
-    # ax1.title.set_text(f'original')
-    # plt.hist(height, bins=bins)
-    # plt.xlabel('z (height)')
-
-    # mask_elevation = [h for h in mask_elevation.flatten() if h != 0]
-    # ax2 = plt.subplot(212, sharex=ax1, sharey=ax1)
-    # ax2.title.set_text(f'mask, sigma:{sigma}')
-    # plt.hist(mask_elevation, bins=bins)
-    # plt.xlabel('z (height)')
-
-    # plt.savefig(f'output/outlier_z.png')
-    # print(f'output/outlier_z.png are saved')
+    # mask_mask = np.invert(np.logical_and(mask, blank_mask))
+    # blank_mask = (mask >= 1) * k + np.multiply(blank_mask, mask_mask)
         
+    # blank_mask = blank_mask * elevation
+    # mask_mask = np.invert(mask_mask) * 1
+    # fig, ax = plt.subplots(dpi=500)
+    # im = ax.imshow(blank_mask, cmap='viridis', vmin=blank_mask.min(), vmax=blank_mask.max())
+    # cmap = copy.copy(mpl.cm.get_cmap("rainbow"))
+    # cmap.set_under('w', alpha=0)
+    # ax.imshow(mask_mask * mask_mask, cmap=cmap, alpha=1, vmin=mask_mask.min() + 0.001)
+    # plt.savefig('output/overlap2.png')
+         
 
-def plot_dem_label(blank_elevation, label):
+def check_dem_label(blank_elevation, label):
     # blank_elevation = np.multiply(blank_elevation, label)
     blank_elevation = standard_deviation(blank_elevation.ravel(), 9).reshape(blank_elevation.shape)
     # fig, ax = plt.subplots(dpi=500)
@@ -273,71 +257,69 @@ def plot_dem_label(blank_elevation, label):
     ax_y.scatter(y, np.arange(len(y)), color=cmap(y_label)[:, :3], s=0.2)
     plt.savefig('output/dem_from_point_cloud.png')
 
+def las_to_elevation():
+    las = laspy.read("stratmap18-50cm_2995222a1output(2).las")
+    point_data_1 = np.stack([las.x, las.y, las.z, las.classification], axis=0).transpose((1, 0))
+
+    las = laspy.read("stratmap18-50cm_2995222a3output(2).las")
+    point_data_2 = np.stack([las.x, las.y, las.z, las.classification], axis=0).transpose((1, 0))
+
+    point_data = np.vstack((point_data_1, point_data_2))
+
+    point_min = np.min(point_data, axis=0)
+    delta =  np.max(point_data, axis=0) - np.min(point_data, axis=0)
+    print(delta * 2)
+    print(point_data.shape)
+
+    label = np.load('label.npy')
+    print(label.shape)
+
+    lowerbound = 10.3
+    upperbound = 35
+
+    keep_point = []
+    remove_point = []
+
+    for i, point in enumerate(tqdm(point_data)):
+        coord = ((point - point_min) * 2).astype(int)
+        point[3] = int(label[label.shape[0] - coord[1] - 1, coord[0]])
+        # if point[3] in [0, 4, 6]:
+        #     if point[2] < lowerbound:
+        #         keep_point.append(point)
+        #     else:
+        #         remove_point.append(point)
+        # elif point[3] in [2, 8]:
+        #     if point[2] > lowerbound and point[2] < upperbound:
+        #         keep_point.append(point)
+        #     else:
+        #         remove_point.append(point)
+    
+    keep_point = np.array(keep_point)
+    remove_point = np.array(remove_point)
+
+    elevation = np.zeros(label.shape)
+    for point in tqdm(point_data):
+        # print(point)
+        coord = ((point - point_min) * 2).astype(int)
+        elevation[label.shape[0] - coord[1] - 1, coord[0]] = point[2]
+    np.save('elevation.npy', elevation.astype(float))
+
+def point_visualization(point):
+    geom = open3d.geometry.PointCloud()
+    geom.points = open3d.utility.Vector3dVector(point[:, :3])
+    cmap = mpl.colors.ListedColormap(["gray", "black", "yellow", "black", "lime", "black", "red", "blue", "green"])
+    geom.colors = open3d.utility.Vector3dVector(cmap(point[:, 3].astype(int))[:, :3])
+    
+    # open3d.visualization.draw_geometries([geom])
+    open3d.io.write_point_cloud('label.pcd', geom)
 
 def main():
-    # las = laspy.read("stratmap18-50cm_2995222a1output(2).las")
-    # point_data_1 = np.stack([las.x, las.y, las.z, las.classification], axis=0).transpose((1, 0))
-
-    # las = laspy.read("stratmap18-50cm_2995222a3output(2).las")
-    # point_data_2 = np.stack([las.x, las.y, las.z, las.classification], axis=0).transpose((1, 0))
-
-    # point_data = np.vstack((point_data_1, point_data_2))
-
-    # point_min = np.min(point_data, axis=0)
-    # delta =  np.max(point_data, axis=0) - np.min(point_data, axis=0)
-    # print(delta * 2)
-    # print(point_data.shape)
-
-    # label = np.load('label.npy')
-    # print(label.shape)
-
-    # lowerbound = 10.3
-    # upperbound = 35
-
-    # keep_point = []
-    # remove_point = []
-
-    # for i, point in enumerate(tqdm(point_data)):
-    #     coord = ((point - point_min) * 2).astype(int)
-    #     point[3] = int(label[label.shape[0] - coord[1] - 1, coord[0]])
-    #     # if point[3] in [0, 4, 6]:
-    #     #     if point[2] < lowerbound:
-    #     #         keep_point.append(point)
-    #     #     else:
-    #     #         remove_point.append(point)
-    #     # elif point[3] in [2, 8]:
-    #     #     if point[2] > lowerbound and point[2] < upperbound:
-    #     #         keep_point.append(point)
-    #     #     else:
-    #     #         remove_point.append(point)
-    # keep_point = np.array(keep_point)
-    # remove_point = np.array(remove_point)
-
-    # elevation = np.zeros(label.shape)
-    # for point in tqdm(point_data):
-    #     # print(point)
-    #     coord = ((point - point_min) * 2).astype(int)
-    #     elevation[label.shape[0] - coord[1] - 1, coord[0]] = point[2]
-    # np.save('elevation.npy', elevation.astype(float))
-
     elevation = np.load('elevation.npy')
     label = np.load('label.npy')
-    # plot_dem_label(elevation, label)
 
+    # check_dem_label(elevation, label)
+    # check_boxplot(elevation, label)
     mask_filter(elevation, label)
-    exit()
-    
-
-    # for label in list(label_dict.keys()):
-    #     check_outlier(new_point, label)
-    # check_z(keep_point, remove_point)
-
-    # geom = open3d.geometry.PointCloud()
-    # geom.points = open3d.utility.Vector3dVector(new_point[:, :3])
-    # cmap = mpl.colors.ListedColormap(["gray", "black", "yellow", "black", "lime", "black", "red", "blue", "green"])
-    # geom.colors = open3d.utility.Vector3dVector(cmap(new_point[:, 3].astype(int))[:, :3])
-    # # open3d.visualization.draw_geometries([geom])
-    # open3d.io.write_point_cloud('label.pcd', geom)
 
 
 
