79fa106490657fb69394e108f0a7ae10ef7e72ab
zhuluntsai <william041107@gmail.com>
2023-10-12 15:17:52 -0500

    threshold update

--- gps_extract.py
+++ gps_extract.py
@@ -1,10 +1,12 @@
 import pandas as pd
 from tqdm import tqdm 
 import os
+import matplotlib.pyplot as plt
 
 directory = 'Formatted Data'
 
 df_list = []
+speed_list = []
 for root, dirs, files in tqdm(os.walk(directory)):
     for filename in files:
         path = os.path.join(root, filename)
@@ -14,6 +16,13 @@ for root, dirs, files in tqdm(os.walk(directory)):
             df['subject'] = [ p[1] for _ in range(len(df)) ]
             df['date'] = [ p[3] for _ in range(len(df)) ]
             df['trip'] = [ p[4] for _ in range(len(df)) ]
-            df_list.append(df)
 
+            max_speed = df['speed'].max()
+            if max_speed < 2.5:
+                df_list.append(df)
+                speed_list.append(max_speed)
+
+plt.hist(speed_list)
+plt.savefig('test.png')
+# print(speed_list)
 pd.concat(df_list).to_csv('test.csv', index=False)
